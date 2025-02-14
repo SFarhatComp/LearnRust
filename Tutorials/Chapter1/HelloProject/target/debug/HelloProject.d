feea928a9843aca2486a66f77f6373581821a3ac
Sami Farhat <sfarhat4@gmail.com>
2025-02-14 00:00:31 -0500

    Chapter 1 , Understanding FN and HellowWorld

---
+++ Tutorials/Chapter1/HelloProject/target/debug/HelloProject.d
@@ -0,0 +1,1 @@
+/home/miso/Desktop/LearnRust/Tutorials/HelloProject/target/debug/HelloProject: /home/miso/Desktop/LearnRust/Tutorials/HelloProject/src/main.rs
---
+++ Tutorials/Chapter1/HelloProject/target/debug/deps/HelloProject-39a23995ae5f48bb.d
@@ -0,0 +1,5 @@
+/home/miso/Desktop/LearnRust/Tutorials/HelloProject/target/debug/deps/HelloProject-39a23995ae5f48bb: src/main.rs
+
+/home/miso/Desktop/LearnRust/Tutorials/HelloProject/target/debug/deps/HelloProject-39a23995ae5f48bb.d: src/main.rs
+
+src/main.rs:
