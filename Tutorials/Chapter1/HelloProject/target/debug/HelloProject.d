9e45e44c29cd7bc44ba3e8673a0a73f839196ee9
Sami Farhat <sfarhat4@gmail.com>
2025-02-14 00:03:45 -0500

    Added .gitignore to ignore build files

--- Tutorials/Chapter1/HelloProject/target/debug/HelloProject.d
+++
@@ -1,1 +0,0 @@
-/home/miso/Desktop/LearnRust/Tutorials/HelloProject/target/debug/HelloProject: /home/miso/Desktop/LearnRust/Tutorials/HelloProject/src/main.rs
--- Tutorials/Chapter1/HelloProject/target/debug/deps/HelloProject-39a23995ae5f48bb.d
+++
@@ -1,5 +0,0 @@
-/home/miso/Desktop/LearnRust/Tutorials/HelloProject/target/debug/deps/HelloProject-39a23995ae5f48bb: src/main.rs
-
-/home/miso/Desktop/LearnRust/Tutorials/HelloProject/target/debug/deps/HelloProject-39a23995ae5f48bb.d: src/main.rs
-
-src/main.rs:
